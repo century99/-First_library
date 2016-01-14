48e9c9421db278175581b88d55ef28df1ad9b418
century99 <519266299@qq.com>
2016-01-14 10:13:09 +0800

    第一次提交 版本1.0

---
+++ Git_TEXT/Git_TEXT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git_TEXT
+//
+//  Created by lanou3g on 16/1/14.
+//  Copyright © 2016年 wangshiji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
